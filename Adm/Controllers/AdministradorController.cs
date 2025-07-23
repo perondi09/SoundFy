bc5222c2213cc1f53c621fcdd19791d77ac5f3b7
racionalVol1 <guilhermeperondi09@gmail.com>
2025-07-23 14:03:24 -0300

    Refatorando ViewModels

--- Adm/Controllers/AdministradorController.cs
+++ Adm/Controllers/AdministradorController.cs
@@ -1,17 +1,12 @@
 using Adm.ViewModel;
 using Business;
-using Data.Models;
 using Microsoft.AspNetCore.Mvc;
-using Data.ViewModel;
-{
-    
-}
 
 namespace Adm.Controllers
 {
     public class AdministradorController : Controller
     {
-        private readonly AdiministradorBusiness _bus = new AdiministradorBusiness();
+        AdiministradorBusiness AdmBusiness = new AdiministradorBusiness();
 
         //Pagina inicial do administrador
         public IActionResult Index()
@@ -19,47 +14,29 @@ namespace Adm.Controllers
             if (HttpContext.Session.GetString("logado") != "true")
                 return RedirectToAction("Index", "Login");
 
-            var usuarios = _bus.ListarUsuariosPeloId();
-            var usuariosVm = usuarios.Select(u => new UsuarioViewModel
+            var usuariosVm = new List<UsuarioViewModel>();
+            var usuariosModel = AdmBusiness.ListarUsuariosPeloId();
+
+            foreach (var usuario in usuariosModel)
             {
-                Id    = u.Id,
-                Email = u.Email,
-                Tipo  = u.Tipo
-            }).ToList();
-
-            // mapeia estatísticas de artistas
-            var estArt = _bus.ObterEstatisticasArtistas();
-            ViewBag.EstatisticasArtistas = estArt
-                .Select(a => new EstatisticaViewArtistaModel
+                usuariosVm.Add(new UsuarioViewModel
                 {
-                    NomeArtista      = a.NomeArtista,
-                    TotalMusicas     = a.TotalMusicas,
-                    TotalReproducoes = a.TotalReproducoes
-                })
-                .ToList();
-
-            // mapeia estatísticas de músicas
-            var estMus = _bus.ObterEstatisticasMusicas();
-            ViewBag.EstatisticasMusicas = estMus
-                .Select(m => new EstatisticaMusicaViewModel
-                {
-                    Id               = m.Id,
-                    Titulo           = m.Titulo,
-                    NomeArtista      = m.NomeArtista,
-                    TotalReproducoes = m.TotalReproducoes
-                })
-                .ToList();
+                    Id = usuario.Id,
+                    Email = usuario.Email,
+                    Tipo = usuario.Tipo
+                });
+            }
 
             return View(usuariosVm);
         }
 
         //Metodo para excluir um usuario
-        public IActionResult Excluir(int id)
+        public IActionResult Excluir(int Id)
         {
             if (HttpContext.Session.GetString("logado") != "true")
                 return RedirectToAction("Index", "Login");
 
-            _bus.ExcluirUsuarioPeloId(id);
+            AdmBusiness.ExcluirUsuarioPeloId(Id);
             return RedirectToAction("Index");
         }
     }
--- Adm/ViewModel/EstatisticaArtistaViewModel.cs
+++
@@ -1,9 +0,0 @@
-namespace Data.ViewModel
-{
-    public class EstatisticaViewArtistaModel
-    {
-        public string NomeArtista { get; set; }
-        public int TotalMusicas { get; set; }
-        public int TotalReproducoes { get; set; }
-    }
-}
