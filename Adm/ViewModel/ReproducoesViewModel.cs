2366fbfba64b7fca96bddd1b575c328012947b61
Perondi <guilhermeperondi09@gmail.com>
2025-08-25 13:14:10 -0300

    Corrigindo caminhos

--- Adm/ViewModel/ReproducoesViewModel.cs
+++ Adm/ViewModel/ReproducoesViewModel.cs
@@ -3,8 +3,8 @@ namespace Adm.ViewModel
     public class ReproducoesViewModel
     {
         public int Id { get; set; }
-        public string Titulo { get; set; }
-        public string NomeArtista { get; set; }
+        public required string Titulo { get; set; }
+        public required string NomeArtista { get; set; }
         public int Reproducao { get; set; }
     }
 }
--- Adm/ViewModel/UsuarioViewModel.cs
+++ Adm/ViewModel/UsuarioViewModel.cs
@@ -3,8 +3,8 @@ namespace Adm.ViewModel
     public class UsuarioViewModel
     {
         public int Id { get; set; }      
-        public string Email { get; set; }
-        public string Senha { get; set; }
-        public string Tipo { get; set; }
+        public string? Email { get; set; }
+        public string? Senha { get; set; }
+        public string? Tipo { get; set; }
     }
 }
