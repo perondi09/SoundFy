96a35d1c53b00d91aaa84e906b90e2e842661aef
Guilherme <guilhermeperondi09@gmail.com>
2026-07-26 20:19:02 -0300

    troca de stack

--- Data/Models/PlaylistMusicaModel.cs
+++
@@ -1,9 +0,0 @@
-namespace Data.Models
-{
-    public class PlaylistMusicaModel
-    {
-        public int Id { get; set; }
-        public int Playlist_Id { get; set; }
-        public int Musica_Id { get; set; }
-    }
-}
